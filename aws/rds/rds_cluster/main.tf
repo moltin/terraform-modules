9b6532d2695c9229bdab13473c646af6621d1fce
Israel Sotomayor <zot24@users.noreply.github.com>
2017-05-19 11:54:43 +0100

    chg: add backup and maintenance options

--- aws/rds/rds_cluster/main.tf
+++ aws/rds/rds_cluster/main.tf
@@ -2,12 +2,20 @@
  * This modules create an [AWS RDS Cluster](https://www.terraform.io/docs/providers/aws/r/rds_cluster.html)
  */
 
+variable "backup_retention_period" {
+    description = "The backup retention period"
+}
+
 variable "database_name" {
     description = "The name for your database of up to 8 alpha-numeric characters. If you do not provide a name, Amazon RDS will not create a database in the DB cluster you are creating"
 }
 
 variable "db_subnet_group_name" {
     description = "A DB subnet group to associate with this DB instance. NOTE: This must match the db_subnet_group_name specified on every aws_rds_cluster_instance in the cluster"
+}
+
+variable "final_snapshot_identifier" {
+    description = "The name of your final DB snapshot when this DB cluster is deleted. If omitted, no final snapshot will be made"
 }
 
 variable "master_password" {
@@ -27,6 +35,14 @@ variable "port" {
     description = "The port on which the DB accepts connections"
 }
 
+variable "preferred_backup_window" {
+    description = "The time window on which backups will be made (HH:mm-HH:mm)"
+}
+
+variable "preferred_maintenance_window" {
+    description = "The weekly time range during which system maintenance can occur, in (UTC) e.g. wed:04:00-wed:04:30"
+}
+
 variable "skip_final_snapshot" {
     default = false
     description = "Determines whether a final DB snapshot is created before the DB instance is deleted. If true is specified, no DBSnapshot is created. If false is specified, a DB snapshot is created before the DB instance is deleted, using the value from final_snapshot_identifier"
@@ -39,13 +55,17 @@ variable "vpc_security_group_ids" {
 resource "aws_rds_cluster" "mod" {
     cluster_identifier = "${var.name}-db"
 
-    port                   = "${var.port}"
-    database_name          = "${var.database_name}"
-    master_username        = "${var.master_username}"
-    master_password        = "${var.master_password}"
-    skip_final_snapshot    = "${var.skip_final_snapshot}"
-    db_subnet_group_name   = "${var.db_subnet_group_name}"
-    vpc_security_group_ids = ["${var.vpc_security_group_ids}"]
+    port                         = "${var.port}"
+    database_name                = "${var.database_name}"
+    master_username              = "${var.master_username}"
+    master_password              = "${var.master_password}"
+    skip_final_snapshot          = "${var.skip_final_snapshot}"
+    db_subnet_group_name         = "${var.db_subnet_group_name}"
+    vpc_security_group_ids       = ["${var.vpc_security_group_ids}"]
+    backup_retention_period      = "${var.backup_retention_period}"
+    preferred_backup_window      = "${var.preferred_backup_window}"
+    final_snapshot_identifier    = "${var.final_snapshot_identifier}"
+    preferred_maintenance_window = "${var.preferred_maintenance_window}"
 }
 
 // The database port
